151c5a80baf234ae9bffb0261d0f8b1f744a0923
FahimFBA <fahimbinamin@gmail.com>
2024-12-12 11:15:10 +0600

    solve 2 from HW 5

---
+++ HomeWork-5/Solve/2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("My name is Fahim\n");
+    printf("My faculty is Md. Fahim Bin Amin.\n");
+    printf("I am learning \"C\" programming language.\n");
+    printf("/\\C/\\ Programming language is easier to understand!\n");
+    return 0;
+}
