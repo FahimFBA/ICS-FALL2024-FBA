9ab5a43981d00c1be8edf335f25341b63607d5e6
FahimFBA <fahimbinamin@gmail.com>
2025-01-03 00:46:20 +0600

    Create 5.c

---
+++ HomeAssignment-1/Questions/basic-problemset-solution/5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b; // declare two integer variables
+    a = 18, b = 7; // initialize the integer variables
+
+    printf("%d + %d = %d\n", a, b, a + b); // print the sum of two integers
+    printf("%d - %d = %d\n", a, b, a - b); // print the difference of two integers
+    printf("%d * %d = %d\n", a, b, a * b); // print the product of two integers
+    printf("%d / %d = %d\n", a, b, a / b); // print the division of two integers
+    printf("%d %% %d = %d\n", a, b, a % b); // print the remainder of two integers
+
+    // you can create separate variables for the sum, difference, product, division, and remainder
+}
