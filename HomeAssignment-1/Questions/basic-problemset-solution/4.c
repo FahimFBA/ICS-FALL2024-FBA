03fed3260a0763e8be330064e75de7f5f3ecde63
FahimFBA <fahimbinamin@gmail.com>
2025-01-03 00:44:11 +0600

    Create 4.c

---
+++ HomeAssignment-1/Questions/basic-problemset-solution/4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a; // declare an integer variable
+    float b; // declare a float variable
+    char c; // declare a character variable
+
+    a = 17; // initialize the integer variable
+    b = 3.508; // initialize the float variable
+    c = 'W'; // initialize the character variable
+
+    printf("Integer value = %d\n", a); // print the integer value
+    printf("Floating point value = %.3f\n", b); // print the float value
+    printf("Character value = %c\n", c); // print the character value
+}
