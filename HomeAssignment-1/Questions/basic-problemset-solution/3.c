1558898863ea9853616b164aea15ff9ec469d5b6
FahimFBA <fahimbinamin@gmail.com>
2025-01-03 00:42:05 +0600

    Create 3.c

---
+++ HomeAssignment-1/Questions/basic-problemset-solution/3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("I am a student of Computer Science and Engineering.\nI am learning C programming.\nI am enjoying this course.\n");
+
+    // you can write anything in this answer
+}
